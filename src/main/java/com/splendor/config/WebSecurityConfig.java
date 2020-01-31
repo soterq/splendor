7d041ffd04e66026611f365bf15ecab99afa553c
VitalieS <VitalieS@gilat.com>
2020-01-31 17:19:05 +0200

    added authO2 finish

--- src/main/java/com/splendor/config/WebSecurityConfig.java
+++ src/main/java/com/splendor/config/WebSecurityConfig.java
@@ -20,9 +20,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/**")
                 .authorizeRequests()
-                .mvcMatchers("/").permitAll()
+                .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
                 .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
                 .and()
                 .csrf().disable();
     }
@@ -31,8 +33,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
         return map -> {
             String id = (String) map.get("sub");
+
             User user = userDetailsRepo.findById(id).orElseGet(() -> {
                 User newUser = new User();
+
                 newUser.setId(id);
                 newUser.setName((String) map.get("name"));
                 newUser.setEmail((String) map.get("email"));
@@ -40,9 +44,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 newUser.setLocale((String) map.get("locale"));
                 newUser.setUserpic((String) map.get("picture"));
 
-                return new User();
+                return newUser;
             });
+
             user.setLastVisit(LocalDateTime.now());
+
             return userDetailsRepo.save(user);
         };
     }
---
+++ src/main/java/com/splendor/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.splendor.controller;
+
+import com.splendor.domain.User;
+import com.splendor.repo.MessageRepo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    private final MessageRepo messageRepo;
+
+
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    @GetMapping
+    public String main(Model model, @AuthenticationPrincipal User user) {
+        HashMap<Object, Object> data = new HashMap<>();
+
+        data.put("profile", user);
+        data.put("messages", messageRepo.findAll());
+
+        model.addAttribute("frontendData", data);
+
+        return "index";
+    }
+}
